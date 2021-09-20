1cce1181e568cec828134434c1deb10291d1baaa
Lars Høgedal <lars.hogedal@gmail.com>
2021-09-20 11:35:25 +0200

    updated link to conform with new csv

--- Domain/Lifecourse/Link.cs
+++ Domain/Lifecourse/Link.cs
@@ -1,22 +1,30 @@
-﻿using Newtonsoft.Json;
+﻿using CsvHelper.Configuration.Attributes;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace Linklives.Domain
 {
     public partial class Link : KeyedItem
     {
+        [Name("link_id")]
         public int Link_id { get; set; }
+        [Name("iteration")]
         public int Iteration { get; set; }
+        [Name("iteration_inner")]
         public int Iteration_inner { get; set; }
+        [Name("method_id")]
         public int Method_id { get; set; }
+        [Name("pa_id1")]
         public int Pa_id1 { get; set; }
+        [Name("score")]
         public double Score { get; set; }
+        [Name("pa_id2")]
         public int Pa_id2 { get; set; }
+        [Name("source_id1")]
         public int Source_id1 { get; set; }
+        [Name("source_id2")]
         public int Source_id2 { get; set; }
-        public string Method_type { get; set; }
-        public string Method_subtype1 { get; set; }
-        public string Method_description { get; set; }
+        [Ignore]
         public string LifeCourseKey { get; set; }
         [JsonIgnore]
         public virtual LifeCourse LifeCourse { get; set; }
