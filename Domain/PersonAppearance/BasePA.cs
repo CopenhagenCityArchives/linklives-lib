c9444e4ae8dbfbc1ab784344f4b2eef61b1e7cf9
Lars Høgedal <lars.hogedal@gmail.com>
2021-09-24 10:24:40 +0200

    Implemented BasePA mapping

--- Domain/PersonAppearance/BasePA.cs
+++ Domain/PersonAppearance/BasePA.cs
@@ -1,6 +1,8 @@
 ﻿using Nest;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
 using System.Text;
 
 namespace Linklives.Domain
@@ -10,6 +12,8 @@ namespace Linklives.Domain
     /// </summary>
     public abstract class BasePA : KeyedItem
     {
+        private object source_type_wp4;
+
         public int Source_id { get; set; }
         public int Pa_id { get; set; }
         public SourceType Type { get; protected set; }
@@ -25,9 +29,9 @@ namespace Linklives.Domain
         public string Birthyear_searchable_fz { get; set; }
         public string Birthplace_searchable { get; set; }
         public int Sourceyear_searchable { get; set; }
-        public string sourceyear_searchable_fz { get; set; }
+        public string Sourceyear_searchable_fz { get; set; }
         public string Sourceplace_searchable { get; set; }
-        public int Deathyear_searchable { get; set; }
+        public int? Deathyear_searchable { get; set; }
         public string Deathyear_searchable_fz { get; set; }
         public string Gender_searchable { get; set; }
         public string Birthname_searchable { get; set; }
@@ -45,15 +49,15 @@ namespace Linklives.Domain
 
         //Display
         public string Name_display { get; set; }
-        public string Birthyear_display { get; set; }
+        public int Birthyear_display { get; set; }
         public string Role_display { get; set; }
         public string Birthplace_display { get; set; }
         public string Occupation_display { get; set; }
         public string Sourceplace_display { get; set; }
         public string Event_type_display { get; set; }
-        public string Sourceyear_display { get; set; }
+        public int Sourceyear_display { get; set; }
         public string Event_year_display { get; set; }
-        public string Deathyear_display { get; set; }
+        public int? Deathyear_display { get; set; }
         public string Source_type_display { get; set; }
         public string Source_archive_display { get; set; }
 
@@ -94,7 +98,70 @@ namespace Linklives.Domain
         }
         protected virtual void InitFields()
         {
-            //TODO: Implement general mapping
+            Name_searchable = Standard.Name_cl;
+            Name_searchable_fz = $"{Standard.Name_cl} {Standard.Name}";
+            Lastname_searchable = Standard.Name_cl.Split(' ').Last();
+            Lastname_searchable_fz = string.Join(' ', new string[]
+                { Lastname_searchable }
+                .Concat(Standard.Patronyms.Split(' '))
+                .Concat(Standard.Family_names.Split(' '))
+                .Concat(Standard.Maiden_names.Split(' '))
+                .Concat(Standard.All_patronyms.Split(' '))
+                .Concat(Standard.All_family_names.Split(' '))
+                .Distinct());
+            Firstnames_searchable = Standard.Name_cl.Substring(0, Standard.Name_cl.Length - Lastname_searchable.Length + 1); //+1 to also get the preceding space
+            Firstnames_searchable_fz = string.Join(' ', new string[]
+                { Firstnames_searchable }
+                .Concat(Standard.First_names.Split(' '))
+                .Distinct());
+            Birthyear_searchable = Convert.ToInt32(Standard.Birth_year);
+            Birthyear_searchable_fz = string.Join(' ', new int[]
+                {
+                    Birthyear_searchable -3,
+                    Birthyear_searchable -2,
+                    Birthyear_searchable -1,
+                    Birthyear_searchable,
+                    Birthyear_searchable +1,
+                    Birthyear_searchable +2,
+                    Birthyear_searchable +3
+                });
+            Birthplace_searchable = string.Join(' ', new string[] { Standard.Birth_place, Standard.Birth_location, Standard.Birth_parish, Standard.Birth_town, Standard.Birth_county, Standard.Birth_country, Standard.Birth_foreign_place });
+            Sourceyear_searchable = Convert.ToInt32(Standard.Event_year);
+            Sourceyear_searchable_fz = string.Join(' ', new int[]
+                {
+                    Sourceyear_searchable -3,
+                    Sourceyear_searchable -2,
+                    Sourceyear_searchable -1,
+                    Sourceyear_searchable,
+                    Sourceyear_searchable +1,
+                    Sourceyear_searchable +2,
+                    Sourceyear_searchable +3
+                });
+            Sourceplace_searchable = null; //To be filled by derived class
+            Deathyear_searchable = null; //To be filled by derived class
+            Deathyear_searchable_fz = null; //To be filled by derived class
+            Gender_searchable = string.IsNullOrEmpty(Standard.Sex) ? "u" : Standard.Sex;
+            Birthname_searchable = Standard.Maiden_names;
+            Occupation_searchable = null; //To be filled by derived class
+
+            First_names_sortable = Firstnames_searchable;
+            Family_names_sortable = Lastname_searchable;
+            Birthyear_sortable = Birthyear_searchable;
+
+            Last_updated_wp4 = DateTime.Now;
+            Pa_entry_permalink_wp4 = ""; //TODO: figure out whats supposed to go in this field
+
+            Name_display = string.Join(' ', Standard.Name_cl.Split(' ').Select(s => s[0].ToString().ToUpper() + s.Substring(1))); //Make first letter of each word uppercase
+            Birthyear_display = Birthyear_searchable;
+            Role_display = PAStrings.ResourceManager.GetString(Standard.Role.ToLower()) ?? Standard.Role;
+            Birthplace_display = Birthplace_searchable;
+            Occupation_display = null; //To be filled by derived class
+            Sourceplace_display = null; //To be filled by derived class
+            Event_type_display = PAStrings.ResourceManager.GetString(Standard.Event_type.ToLower()) ?? Standard.Event_type;
+            Sourceyear_display = Convert.ToInt32(Standard.Event_year);
+            Deathyear_display = Deathyear_searchable;
+            Source_type_display = null; //To be filled by derived class
+            Source_archive_display = null; //To be filled by derived class
         }
         public override void InitKey()
         {
