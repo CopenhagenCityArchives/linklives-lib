8ec5f99f93a8f5c94066cced382225825bb95458
Lars Høgedal <lars.hogedal@gmail.com>
2021-10-19 09:16:16 +0200

    Added new object to wrap transcribed PAs so we can add keys to them

---
+++ Domain/PersonAppearance/TranscribedPA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linklives.Domain.PersonAppearance
+{
+    public class TranscribedPA : KeyedItem
+    {
+        public int Pa_id { get; set; }
+        public int Source_id { get; set; }
+        public dynamic Transcription { get; set; }
+        public override void InitKey()
+        {
+            Key = $"{Source_id}-{Pa_id}";
+        }
+    }
+}
