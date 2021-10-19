20c67816e3ab150033ef12a68178da63b961a6a5
Lars Høgedal <lars.hogedal@gmail.com>
2021-10-19 11:41:32 +0200

    Added repo for transcribed pa's

---
+++ DAL/ESTranscribedPaRepository.cs
@@ -0,0 +1,32 @@
+﻿using Linklives.Domain;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linklives.DAL
+{
+    public class ESTranscribedPaRepository : ITranscribedPARepository
+    {
+        private readonly ElasticClient client;
+
+        public ESTranscribedPaRepository(ElasticClient client)
+        {
+            this.client = client;
+        }
+
+        public IEnumerable<TranscribedPA> GetBySource(int sourceId)
+        {
+            var searchResponse = client.Search<TranscribedPA>(s => s
+                .Index("Transcribed")
+                .From(0)
+                .Size(int.MaxValue)
+                .Query(q => q
+                    .Match(m => m
+                        .Field(f => f.Source_id)
+                        .Query(sourceId.ToString()))));
+            return searchResponse.Documents;
+
+        }
+    }
+}
---
+++ DAL/ITranscribedPARepository.cs
@@ -0,0 +1,12 @@
+﻿using Linklives.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linklives.DAL
+{
+    public interface ITranscribedPARepository
+    {
+        IEnumerable<TranscribedPA> GetBySource(int sourceId);
+    }
+}
