f1f78b0cd9112cc0c3bba01834a7ca116d304ad8
Lars Høgedal <lars.hogedal@gmail.com>
2021-10-20 11:08:04 +0200

    reconfigured file structure

---
+++ linklives-dummy/LinklivescontextFacotry.cs
@@ -0,0 +1,19 @@
+﻿using Linklives.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linklives_dummy
+{
+    class LinklivesContextFactory : IDesignTimeDbContextFactory<LinklivesContext>
+    {
+        public LinklivesContext CreateDbContext(string[] args)
+        {
+            var dbConn = @"server=***REMOVED***;***REMOVED***;pwd=***REMOVED***;database=***REMOVED***";
+            var dbContext = new LinklivesContext(new DbContextOptionsBuilder<LinklivesContext>().UseMySQL(dbConn).Options);
+            return dbContext;
+        }
+    }
+}
---
+++ linklives-dummy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace linklives_dummy
+{
+    //This project serves as a dummy executable to use when generating new database migrations with EF core
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
