b55e9fe042e984ce58777598e3dceff8acd1ed63
Lars Høgedal <lars.hogedal@gmail.com>
2021-10-27 15:16:43 +0200

    use property syntax instead of dictionary syntax for transcribed fields

--- linklives-lib/Domain/PersonAppearance/ParishPA.cs
+++ linklives-lib/Domain/PersonAppearance/ParishPA.cs
@@ -19,11 +19,11 @@ namespace Linklives.Domain
         {
             if (string.IsNullOrEmpty(Birthplace_searchable))
             {
-                Birthplace_searchable = string.Join(' ', new string[] { Transcribed.Transcription["BirthPlace"], Transcribed.Transcription["BirthParish"], Transcribed.Transcription["BirthMunicilality"], Transcribed.Transcription["BirthState"] });
+                Birthplace_searchable = string.Join(' ', new string[] { Transcribed.Transcription.BirthPlace., Transcribed.Transcription.BirthParish., Transcribed.Transcription.BirthMunicilality, Transcribed.Transcription.BirthState });
             }
             if (string.IsNullOrEmpty(Sourceplace_searchable))
             {
-                Sourceplace_searchable = string.Join(' ', new string[] { Transcribed.Transcription["Browselevel"], Transcribed.Transcription["Browselevel1"] }).Trim();
+                Sourceplace_searchable = string.Join(' ', new string[] { Transcribed.Transcription.Browselevel, Transcribed.Transcription.Browselevel1 }).Trim();
             }
             if (Standard.Event_type == "burial")
             {
@@ -42,10 +42,10 @@ namespace Linklives.Domain
             }
             if (string.IsNullOrEmpty(Birthplace_display))
             {
-                Birthplace_searchable = string.Join(' ', new string[] { Transcribed.Transcription["BirthPlace"], string.IsNullOrEmpty(Transcribed.Transcription["BirthParish"]) ? null : Transcribed.Transcription["BirthParish"] + " sogn", Transcribed.Transcription["BirthMunicipality"], Transcribed.Transcription["BirthState"] }).Trim().Replace(' ', ','); //trim and replace so we dont end up with strings of just commas
+                Birthplace_searchable = string.Join(' ', new string[] { Transcribed.Transcription.BirthPlace, string.IsNullOrEmpty(Transcribed.Transcription.BirthParish) ? null : Transcribed.Transcription.BirthParish + " sogn", Transcribed.Transcription.BirthMunicipality, Transcribed.Transcription.BirthState }).Trim().Replace(' ', ','); //trim and replace so we dont end up with strings of just commas
             }
-            Sourceplace_display = string.Join(' ', new string[] { Transcribed.Transcription["Browselevel1"], Transcribed.Transcription["Browselevel"] }).Trim().Replace(' ', ',');  //trim and replace so we dont end up with strings of just commas
-            Sourceyear_display = Transcribed.Transcription["Browselevel2"];
+            Sourceplace_display = string.Join(' ', new string[] { Transcribed.Transcription.Browselevel1, Transcribed.Transcription.Browselevel }).Trim().Replace(' ', ',');  //trim and replace so we dont end up with strings of just commas
+            Sourceyear_display = Transcribed.Transcription.Browselevel2;
             Event_year_display = Standard.Event_year;
             Deathyear_display = Deathyear_searchable;
             Source_type_display = "Kirkebog";
