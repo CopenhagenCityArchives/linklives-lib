251492ad6b60859dac9cc7860e26af0ccc710e2f
Lars Høgedal <lars.hogedal@gmail.com>
2021-10-27 12:17:22 +0200

    added constructors to TranscribedPa

--- linklives-lib/Domain/PersonAppearance/TranscribedPA.cs
+++ linklives-lib/Domain/PersonAppearance/TranscribedPA.cs
@@ -9,6 +9,16 @@ namespace Linklives.Domain
         public int Pa_id { get; set; }
         public int Source_id { get; set; }
         public dynamic Transcription { get; set; }
+        public TranscribedPA()
+        {
+
+        }
+        public TranscribedPA(dynamic transcription, int sourceId)
+        {
+            Pa_id = Convert.ToInt32(transcription.pa_id);
+            Source_id = sourceId;
+            Transcription = transcription;
+        }
         public override void InitKey()
         {
             Key = $"{Source_id}-{Pa_id}";
