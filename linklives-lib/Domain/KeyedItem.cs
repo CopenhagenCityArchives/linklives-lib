cf75bf8be4c099aeb1be547bde55b17e525ad294
Bo Henriksen <stumpdenmark@hotmail.com>
2021-11-11 11:43:44 +0100

    Update KeyedItem.cs
    Key is now a keyword in the Elasticsearch mapping

--- linklives-lib/Domain/KeyedItem.cs
+++ linklives-lib/Domain/KeyedItem.cs
@@ -6,6 +6,7 @@ namespace Linklives.Domain
     {
         [CsvHelper.Configuration.Attributes.Ignore]
         [Column(TypeName ="Varchar(350)")] //If we dont specify the field size we cant use composite keys because they become too big
+        [Nest.Keyword]
         public string Key { get; protected set; }        
         public abstract void InitKey();
     }
