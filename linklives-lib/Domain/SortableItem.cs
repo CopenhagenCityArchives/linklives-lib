5c32ed2ccd9cbbd510124afd995ca2fe748b5da9
Bo Henriksen <stumpdenmark@hotmail.com>
2021-12-07 21:16:06 +0100

    SortableItems now has keyword properties which is also not mapped by EF

--- linklives-lib/Domain/SortableItem.cs
+++ linklives-lib/Domain/SortableItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Linklives.Domain
@@ -8,16 +9,28 @@ namespace Linklives.Domain
     {
         // Sortables
         [CsvHelper.Configuration.Attributes.Ignore]
+        [NotMapped]
+        [Nest.Keyword]
         public abstract int? Sourceyear_sortable { get; }
         [CsvHelper.Configuration.Attributes.Ignore]
+        [NotMapped]
+        [Nest.Keyword]
         public abstract string First_names_sortable { get; }
         [CsvHelper.Configuration.Attributes.Ignore]
+        [NotMapped]
+        [Nest.Keyword]
         public abstract string Family_names_sortable { get; }
         [CsvHelper.Configuration.Attributes.Ignore]
+        [NotMapped]
+        [Nest.Keyword]
         public abstract int? Birthyear_sortable { get; }
         [CsvHelper.Configuration.Attributes.Ignore]
+        [NotMapped]
+        [Nest.Keyword]
         public abstract int? Event_year_sortable { get; }
         [CsvHelper.Configuration.Attributes.Ignore]
+        [NotMapped]
+        [Nest.Keyword]
         public abstract int? Deathyear_sortable { get; }
     }
 }
