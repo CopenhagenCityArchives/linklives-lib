b6891598fd3da910ef0cc916a414521ebc068bd8
Bo Henriksen <stumpdenmark@hotmail.com>
2021-12-13 09:07:03 +0100

    Add LifeCourseLink as DbSet in LinkLivesContext

---
+++ linklives-lib/Domain/Lifecourse/LifeCourseLink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linklives.Domain
+{
+    public class LifeCourseLink : KeyedItem
+    {
+        public string LifeCoursesKey { get; set; }
+        public string LinksKey { get; set; }
+        public virtual LifeCourse LifeCourse { get; set; }
+        public virtual Link Link { get; set; }
+
+        public override void InitKey() { }
+    }
+}
