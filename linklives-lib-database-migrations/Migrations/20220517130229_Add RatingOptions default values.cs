46ccb1463d7b946acae7e14df7f388562c2c0cc4
Bo Henriksen <stumpdenmark@hotmail.com>
2022-05-17 15:04:29 +0200

    Add default values for RatingOptions

---
+++ linklives-lib-database-migrations/Migrations/20220517130229_Add RatingOptions default values.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Linklives.Migrations
+{
+    public partial class AddRatingOptionsdefaultvalues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "RatingOptions",
+                columns: new[] { "Id", "Category", "Heading", "Text" },
+                values: new object[,]
+                {
+                    { 1, "positive", "Ja, det er troværdigt", "Det ser fornuftigt ud. Personinformationen i de to kilder passer sammen." },
+                    { 2, "positive", "Ja, det er troværdigt", "Jeg kan bekræfte informationen fra andre kilder, der ikke er med i Link-Lives." },
+                    { 3, "positive", "Ja, det er troværdigt", "Jeg kan genkende informationen fra min private slægtsforskning." },
+                    { 4, "negative", "Nej, det er ikke troværdigt", "Det ser forkert ud. Personinformation i de to kilder passer ikke sammen." },
+                    { 5, "negative", "Nej, det er ikke troværdigt", "Jeg ved det er forkert ud fra andre kilder, der ikke er med i Link-Lives." },
+                    { 6, "negative", "Nej, det er ikke troværdigt", "Jeg ved det er forkert fra min private slægtsforskning." },
+                    { 7, "neutral", "Måske", "Jeg er i tvivl om personinformationen i de to kilder passer sammen." },
+                    { 8, "neutral", "Måske", "Nogle af informationerne passer sammen. Andre gør ikke." },
+                    { 9, "neutral", "Måske", "Der er ikke personinformation nok til at afgøre, om det er troværdigt." }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "RatingOptions",
+                keyColumn: "Id",
+                keyValue: 9);
+        }
+    }
+}
--- linklives-lib/Domain/LinklivesContext.cs
+++ linklives-lib/Domain/LinklivesContext.cs
@@ -40,6 +40,17 @@ namespace Linklives.Domain
                 entity.HasKey(x => x.Id);
             });
 
+            modelBuilder.Entity<RatingOption>().HasData(
+                new RatingOption() { Id = 1, Text = "Det ser fornuftigt ud. Personinformationen i de to kilder passer sammen.", Heading = "Ja, det er troværdigt", Category = "positive" },
+                new RatingOption() { Id = 2, Text = "Jeg kan bekræfte informationen fra andre kilder, der ikke er med i Link-Lives.", Heading = "Ja, det er troværdigt", Category = "positive" },
+                new RatingOption() { Id = 3, Text = "Jeg kan genkende informationen fra min private slægtsforskning.", Heading = "Ja, det er troværdigt", Category = "positive" },
+                new RatingOption() { Id = 4, Text = "Det ser forkert ud. Personinformation i de to kilder passer ikke sammen.", Heading = "Nej, det er ikke troværdigt", Category = "negative" },
+                new RatingOption() { Id = 5, Text = "Jeg ved det er forkert ud fra andre kilder, der ikke er med i Link-Lives.", Heading = "Nej, det er ikke troværdigt", Category = "negative" },
+                new RatingOption() { Id = 6, Text = "Jeg ved det er forkert fra min private slægtsforskning.", Heading = "Nej, det er ikke troværdigt", Category = "negative" },
+                new RatingOption() { Id = 7, Text = "Jeg er i tvivl om personinformationen i de to kilder passer sammen.", Heading = "Måske", Category = "neutral" },
+                new RatingOption() { Id = 8, Text = "Nogle af informationerne passer sammen. Andre gør ikke.", Heading = "Måske", Category = "neutral" },
+                new RatingOption() { Id = 9, Text = "Der er ikke personinformation nok til at afgøre, om det er troværdigt.", Heading = "Måske", Category = "neutral" }
+            );
         }
     }
 }
