52a5f88a4a689d0286ce9201a427a97e2d65721e
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-12-06 16:07:57 +0100

    Add DAL for download history

---
+++ linklives-lib/DAL/EFDownloadHistoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Linklives.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linklives.DAL
+{
+    public interface IEFDownloadHistoryRepository {
+        void RegisterDownload(DownloadHistoryEntry entry);
+        void Save();
+    }
+
+    public class EFDownloadHistoryRepository : DBRepository<DownloadHistoryEntry>, IEFDownloadHistoryRepository
+    {
+        private readonly DbContextOptions<LinklivesContext> contextOptions;
+        private readonly int batchSize = 1000;
+        public EFDownloadHistoryRepository(LinklivesContext context, DbContextOptions<LinklivesContext> options) : base(context)
+        {
+            contextOptions = options;
+        }
+
+        public void RegisterDownload(DownloadHistoryEntry entry)  {
+            context.DownloadHistoryEntries.Add(entry);
+        }
+    }
+}
---
+++ linklives-lib/Domain/DownloadHistoryEntry.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Linklives.Domain
+{
+
+    public enum DownloadType {
+        PersonAppearance = 0,
+        Lifecourse = 1,
+        SearchResult = 2,
+    }
+
+    public static class DownloadTypeExt {
+        public static string Stringify(this DownloadType type) {
+            switch(type) {
+                case DownloadType.PersonAppearance:
+                    return "PersonAppearance";
+                case DownloadType.Lifecourse:
+                    return "Lifecourse";
+                case DownloadType.SearchResult:
+                    return "SearchResult";
+                default:
+                    return "Download";
+            }
+        }
+    }
+
+    public class DownloadHistoryEntry {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? Id { get; set; }
+
+        [DataMember, Required(AllowEmptyStrings = false, ErrorMessage = "Must have a valid download type")]
+        public string DownloadType { get; set; }
+
+        [DataMember, Required(AllowEmptyStrings = false, ErrorMessage = "Must have valid query")]
+        public string Query { get; set; }
+
+        [DataMember, Required(AllowEmptyStrings = false, ErrorMessage = "Must have valid downloadedBy ID")]
+        public string DownloadedBy { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Created { get; set; }
+
+        public DownloadHistoryEntry(DownloadType downloadType, string query, string downloadedBy) {
+            DownloadType = downloadType.Stringify();
+            Query = query;
+            DownloadedBy = downloadedBy;
+        }
+    }
+}
